1b6cbb4bd617f5233df6d668ce59444e288cca96
linoycohen01 <linoy.cohen01@post.runi.ac.il>
2024-11-24 11:55:05 +0200

    2

--- Algebra.java
+++ Algebra.java
@@ -6,8 +6,8 @@
 public class Algebra {
 	public static void main(String args[]) {
 	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3
-	    System.out.println(minus(7,2));  // 7 - 2
+	    System.out.println(plus(-2,-3));   // 2 + 3
+	    System.out.println(minus(7,-2));  // 7 - 2
    		System.out.println(minus(2,7));  // 2 - 7
  		System.out.println(times(3,4));  // 3 * 4
    		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
@@ -26,8 +26,11 @@ public class Algebra {
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
 		int sum = x1;
-		for (int i = 0; i < x2; i++){
+		for (int i = 0; i < Math.abs(x2); i++){
+			if (x2 > 0)
 			sum ++;
+			else
+			sum--;
 		}
 		return sum;
 	}
@@ -35,8 +38,11 @@ public class Algebra {
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
 		int sum = x1;
-		for (int i = 0; i < x2; i++){
+		for (int i = 0; i < Math.abs(x2); i++){
+			if (x2 > 0)
 			sum --;
+			else 
+				sum++;
 		}
 		return sum;
 	}
@@ -44,7 +50,7 @@ public class Algebra {
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
 		int sum = 0;
-		for (int i = 0; i < x2; i++){
+		for (int i = 0; i < Math.abs(x2); i++){
 			sum = plus(sum , x1);
 		}
 		return sum; 
