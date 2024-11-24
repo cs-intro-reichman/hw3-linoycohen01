9dcc16a018de2c20c242949dd989e86e254cf40f
linoycohen01 <linoy.cohen01@post.runi.ac.il>
2024-11-24 12:32:57 +0200

    5

--- Algebra.java
+++ Algebra.java
@@ -13,9 +13,10 @@ public class Algebra {
    		//System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
    		//System.out.println(pow(-5,3));      // 5^3
    		//System.out.println(pow(3,5));      // 3^5
-   		System.out.println(div(-12,3));   // 12 / 3    
+   		System.out.println(div(-12,-3));   // 12 / 3    
    		System.out.println(div(5,-5));    // 5 / 5  
-   		System.out.println(div(25,7));   // 25 / 7
+		System.out.println(div(-18,3));   // 25 / 7
+		System.out.println(div(25,7));   // 25 / 7
    		//System.out.println(mod(25,7));   // 25 % 7
    		//System.out.println(mod(120,6));  // 120 % 6    
    		//System.out.println(sqrt(36));
@@ -91,11 +92,12 @@ public class Algebra {
 			count ++;
 			sum = minus(sum, Math.abs(x2));
 		}
-		if (x1 < 0 || x2 < 0){
+		if (x1 < 0 && x2 < 0 || x1 > 0 && x2 > 0)
+			return count;
+		else
 			return (times(-1 , count));
-		} else {
-		return count;
-	}}
+	
+	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
